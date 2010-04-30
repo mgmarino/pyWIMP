c2c346ab215855b914ede147640fef7cb647a9a3
Michael <mmarino@gmail.com>
2010-04-30 02:58:39 -0700

    Subtle changes, allowing a new output directory to be specified.

--- pyWIMP/submit_job_data_verify.py
+++ pyWIMP/submit_job_data_verify.py
@@ -7,6 +7,7 @@ import numpy
 # First the vitals of the job
 queue = ["default", "scavenge"]
 sens_home = "/share/home/mgmarino/sensitivity/SensitivityCalculation"
+output_dir = "/share/home/mgmarino/DataVerify"
 path_of_results = sens_home
 cores = 8
 nodes = 2
@@ -18,11 +19,15 @@ all_root_files = numpy.array(all_root_files)
 all_root_files = all_root_files[0::2] # split the size in two
 #Now the particulars of the calculations 
 
-file_basename = "mgm_wimp_many_two" 
+file_basename = "mgm_wimp_data_verify" 
 max_time = 3600*int(hours) + 60*int(minutes)
 max_time -= 60 # allow for some extra padding in the wall clock
 #wimp_list = [100, 80, 60, 40]
 job = 0
+
+if not os.path.isdir(output_dir):
+    os.makedirs(output_dir)
+
 for afile in all_root_files:
     execution_string=""
     execution_string+="""
@@ -30,7 +35,7 @@ trap : SIGINT
 trap "echo TERM" SIGTERM
 cd %s
 time mpirun -np %i -hostfile $PBS_NODEFILE python %s/pyWIMP/mpi_job_engine_test_model.py %s 
-     """ % (sens_home, cores*nodes, sens_home, afile)
+     """ % (output_dir, cores*nodes, sens_home, afile)
     
     print "Submitting job"
     write_handle = os.popen("""
