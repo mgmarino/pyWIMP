95194de15b6b16b9dafdedd0c6950e8675334e77
Michael <mmarino@gmail.com>
2010-05-06 06:24:43 -0700

    Updating to properly normalize.  Also selecting a 'nicer' looking graphics algorithm and switching to log in z.

--- test_scripts/make_WIMP_model_plot.py
+++ test_scripts/make_WIMP_model_plot.py
@@ -1,26 +1,45 @@
 
-import wimp_model
+from pyWIMP.DMModels.base_model import BaseVariables
+import pyWIMP.DMModels.wimp_model as wimp_model
 import ROOT
 import re
-mass_of_wimp = 60
-wm = wimp_model.AllWIMPModels(energy_max=20, mass_of_wimp=mass_of_wimp, time_in_years=5)
+mass_of_wimp = 10
+basevars = BaseVariables(0, 4, 0, 3)
+time = basevars.get_time()
+time.setConstant()
+time.setVal(0)
+wm = wimp_model.WIMPModel(basevars, mass_of_wimp)
 model = wm.get_WIMP_model()
-hist = model.createHistogram("hist", wm.get_energy(), \
+expected_events = model.expectedEvents(ROOT.RooArgSet(basevars.get_energy()))
+hist = model.createHistogram("hist", basevars.get_energy(), \
                           ROOT.RooFit.Binning(200),\
-                          ROOT.RooFit.YVar(wm.get_time(), \
-                          ROOT.RooFit.Binning(40, 0, 3)))
+                          ROOT.RooFit.YVar(basevars.get_time(), \
+                          ROOT.RooFit.Binning(40)))
 hist.SetLineColor(4)
 hist.GetXaxis().CenterTitle()
 hist.GetXaxis().SetTitle("Energy (keV)")
-hist.GetXaxis().SetTitleOffset(1.4)
+hist.GetXaxis().SetTitleOffset(1.5)
 hist.GetYaxis().CenterTitle()
 hist.GetYaxis().SetTitle("Time (years)")
-hist.GetYaxis().SetTitleOffset(1.4)
+hist.GetYaxis().SetTitleOffset(1.5)
+hist.GetYaxis().SetNdivisions(507)
 hist.GetZaxis().CenterTitle()
 hist.GetZaxis().SetTitle("Amplitude (a.u.)")
-hist.GetZaxis().SetTitleOffset(1.3)
+hist.GetZaxis().SetTitle("#frac{dR}{dE} #sigma^{-1} (counts/keV/kg/yr/pb)")
+hist.GetZaxis().SetTitleOffset(1.1)
+hist.GetZaxis().SetNdivisions(509)
+ysize = hist.GetYaxis().GetBinWidth(1)
+xsize = hist.GetXaxis().GetBinWidth(1)
+time = basevars.get_time()
+time_scale = time.getMax() - time.getMin()
+print ysize, xsize
+hist.Scale(expected_events/(xsize*ysize))
 c1 = ROOT.TCanvas()
-hist.Draw("SURF")
+c1.SetLogz()
+c1.SetTheta(22.1822);
+c1.SetPhi(-24.31034);
+hist.Draw("SURF4FB")
+hist.GetZaxis().SetRangeUser(0.5, 1000)
 c1.Update()
 raw_input("Enter")
 c1.Print("WIMPModel.eps")
