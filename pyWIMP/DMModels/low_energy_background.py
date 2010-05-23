5bb54d674a1e8b225e0460e6f66ad0b3324163c7
Michael Marino <mmarino@gmail.com>
2010-05-23 12:28:59 -0700

    Making the background fitting model (used for checking the cuts) act more like the exclusion model.

--- pyWIMP/DMModels/low_energy_background.py
+++ pyWIMP/DMModels/low_energy_background.py
@@ -23,38 +23,71 @@ class FittingModel(BaseModel):
                      ("Ga", 9.659, 0.10, 0.1, 0.04, 0), 
                      ("Zn", 8.979, 0.1, 0.1, 0.04, 0), 
                      ("As", 11.103, 0.1, 0.1, 0.04, 0), 
-                     ("Ge-Low", 1.299, 0.0, 0.1, 0.01, 0), 
-                     ("ZN-Low", 1.10, 0.0, 0.1, 0.01, 0) ]
+                     ("Ge-Low", 1.299, 0.0, 0.1, 0.0, 0), 
+                     ("ZN-Low", 1.10, 0.0, 0.1, 0.0, 0) ]
                      #("Unknown", 0.9, 0.05, 0.1, 0.04, 0), 
         
         self.gamma_list = []
         max = basevars.get_energy().getMax()
         min = basevars.get_energy().getMin()
+        print min, max
         for name,mean,mean_error,sigma, sigma_error, atime in mean_list:
             if mean > max or mean < min: continue
+            print mean
             agamma = gamma_line_model.GammaLineFactory.generate(mean, mean_error, get_sigma(mean*1e3)*1e-3, 
                 sigma_error, atime, basevars,name)
             self.gamma_list.append((agamma, agamma.get_model()))
           
-        self.expm = background_model.FlatWithExponentialModel(basevars)
-        self.exp_pdf = self.expm.get_model()
-        self.final_pdf = self.exp_pdf 
-        self.added_pdf = self.final_pdf
         self.saved_pdf = [] # Hack to keep this from dying
+        self.pdf_list = ROOT.RooArgList()
+        self.coefficienct_list = ROOT.RooArgList()
         for _,gamma in self.gamma_list:
             new_var = ROOT.RooRealVar("%s_ampl" % gamma.GetName(), 
                                       "%s_ampl" % gamma.GetName(), 
-                                      0, 1)
-            #self.saved_pdf.append((self.added_pdf, new_var))
-            name = "%s_%s" % (self.added_pdf.GetName(), gamma.GetName())
-            self.saved_pdf.append((self.added_pdf, new_var))
-            self.final_pdf = ROOT.RooAddPdf(name,
-                                       name, 
-                                       gamma, self.added_pdf, new_var)
-            self.added_pdf = self.final_pdf
+                                      1e-15, 10000)
+            self.pdf_list.add(gamma)
+            self.coefficienct_list.add(new_var)
+            self.saved_pdf.append((gamma, new_var))
+
+        tag = ""
+        self.exp_constant_one = ROOT.RooRealVar("expo_const_one%s" % tag,
+                                            "expo_const_one%s" % tag,
+                                            #1./3, 0, 500)
+                                            -3., -100, -0.5)
+        #self.exp_constant_one.removeMax()
+        self.exp_constant_one.setError(0.5)
+        self.exp_constant_time = ROOT.RooRealVar("expo_const_time_%s" % tag,
+                                            "expo_const_time_%s" % tag,
+                                            -0.2, -1, 0.5)
+
+        self.exp_coef = ROOT.RooRealVar("exp_coef_%s" % tag,
+                                        "exp_coef_%s" % tag,
+                                        1e-15, 20000)
+        self.flat_coef = ROOT.RooRealVar("flat_coef_%s" % tag,
+                                         "flat_coef_%s" % tag,
+                                         1e-15, 10000)
+        # Flat pdf
+        self.time_pdf = ROOT.RooPolynomial("time_pdf_exp_%s" % tag, 
+                                           "time_pdf_exp_%s" % tag, 
+                                           basevars.get_time())
+        self.energy_pdf_flat = ROOT.RooPolynomial("energy_pdf_flat_%s" % tag, 
+                                           "energy_pdf_flat_%s" % tag, 
+                                           basevars.get_energy())
+        self.energy_exp_pdf = ROOT.RooExponential("energy_pdf_exp", 
+                                           "energy_pdf_exp", 
+                                           basevars.get_energy(),
+                                           self.exp_constant_one)
+        self.pdf_list.add(self.energy_pdf_flat)
+        self.coefficienct_list.add(self.flat_coef)
+        self.pdf_list.add(self.energy_exp_pdf)
+        self.coefficienct_list.add(self.exp_coef)
+
+
 
     def get_model(self):
         return self.final_pdf
+    def get_list_components(self):
+        return (self.pdf_list, self.coefficienct_list)
 
 
 class LowEnergyBackgroundModel(FittingModel):
