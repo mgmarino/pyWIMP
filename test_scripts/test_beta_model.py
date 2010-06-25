1b9b3fd3f09b9f1b91ae351f7a7f700f3a869743
Michael Marino <mmarino@gmail.com>
2010-06-25 10:05:44 -0700

    Updating test script to use most recent tritium_decay_model

--- test_scripts/test_beta_model.py
+++ test_scripts/test_beta_model.py
@@ -1,39 +1,44 @@
 import ROOT
 import pyWIMP.DMModels.base_model as base_model
+from pyWIMP.DMModels.tritium_decay_model import TritiumDecayModel
 import sys
 import pyWIMP.WIMPPdfs as pdfs  
 from pyWIMP.utilities.utilities import rescale_frame
 import math
 
 
-exposure_time_in_days = 15
-time_in_days = 3*365
+exposure_time_in_days = 5
+time_in_days = 10*365.25
 kg = 30 
-events = (200*exposure_time_in_days*kg*(1 - math.exp(-math.log(2)*(time_in_days)/(12.36*365.25))))
+upper_energy = 25
+lower_energy = 1 
 
-basevars = base_model.BaseVariables(0,1./365., 0, 20)
+basevars = base_model.BaseVariables(5,time_in_days/365.25, lower_energy, upper_energy)
 time = basevars.get_time()
 energy = basevars.get_energy()
-q_value = ROOT.RooRealVar("q_value", "q_value", 18.6)
-mass_of_electron = ROOT.RooRealVar("mass_of_electron", "mass_of_electron", 511)
+energy.setBins(64)
+time.setBins(64)
 
-model_amp = ROOT.RooRealVar("amp", "amp", events, 1e-15, 100000)
-model = pdfs.MGMBetaDecayFunction("beta", "beta", energy, mass_of_electron, q_value)
+tritium_decay = TritiumDecayModel(basevars, exposure_time_in_days, 200, kg, 0.001)
 
-model_extend = ROOT.RooExtendPdf("extend", "extend", model, model_amp)
+total_background = tritium_decay.get_model() 
 
-data = model_extend.generate(ROOT.RooArgSet(energy))
+total_background.getVariables().writeToStream(ROOT.cout, False)
 
-model_extend.fitTo(data)
+data = total_background.generate(ROOT.RooArgSet(energy, time))
+
+total_background.fitTo(data)
 
 c1 = ROOT.TCanvas()
-frame = energy.frame()
-data.plotOn(frame)
-model_extend.plotOn(frame)
-frame.Draw()
-bin_width = frame.getFitRangeBinW()
-axis = rescale_frame(c1, frame, 1./(bin_width*time_in_days*kg), "Events/keV/kg/d") 
-c1.Update()
-raw_input("E")
+for var in [energy, time]:
+    frame = var.frame()
+    data.plotOn(frame)
+    total_background.plotOn(frame)
+    total_background.plotOn(frame, ROOT.RooFit.Components("*flat_extend*"), ROOT.RooFit.LineStyle(ROOT.kDashed))
+    frame.Draw()
+    bin_width = frame.getFitRangeBinW()
+    axis = rescale_frame(c1, frame, 1./(bin_width*time_in_days*kg), "Events/keV/kg/d") 
+    c1.Update()
+    raw_input("E")
 
 
