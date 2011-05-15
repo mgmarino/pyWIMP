6facd55f9e48747d756d80dcb00c91a09f1a6928
Michael Marino <mmarino@gmail.com>
2011-05-15 19:29:11 +0200

    Updates to gamma module and also to the time-dependent model.

--- pyWIMP/DMModels/gamma_line_model.py
+++ pyWIMP/DMModels/gamma_line_model.py
@@ -6,25 +6,15 @@ import math
 class GammaLineModel(BaseModel):
     def __init__(self, 
                  basevars,
-                 mean,
-                 sigma,
-                 lifetime = None,
-                 live_time = None
+                 gamma_pdf,
+                 name,
+                 lifetime,
+                 live_time
                  ):
         BaseModel.__init__(self, basevars)
 
-
-
-        name = str(self.get_tag()) + "_" + str(mean.getVal())
-        if lifetime:
-            name += "_lt_"
-            name += str(lifetime.getVal()) 
-       
-	    # Gamma pdf
-        self.energy_pdf = ROOT.RooGaussian("gamma_line_%s" % name, 
-                                           "GammaPdf_%s" % name, 
-                                           basevars.get_energy(),
-                                           mean, sigma)
+	# Gamma pdf
+        self.energy_pdf = gamma_pdf
         self.lifetime = lifetime
         if not lifetime:
             self.gamma_pdf = self.energy_pdf
@@ -47,38 +37,56 @@ class GammaLineModel(BaseModel):
                                             self.time_pdf, 1e-8)
             self.time_pdf.SetRegionsOfValidity( live_time )
 
+    @classmethod
+    def with_mean_sigma(cls, basevars, mean, sigma, lifetime = None, live_time = None):
+        name = str(mean.getVal())
+        if lifetime:
+           name += "_lt_"
+           name += str(lifetime.getVal()) 
+        energy_pdf = ROOT.RooGaussian("gamma_line_%s" % name, 
+                                      "GammaPdf_%s" % name, 
+                                      basevars.get_energy(),
+                                      mean, sigma)
+        return cls(basevars, energy_pdf, name, lifetime, live_time)
+
+    @classmethod
+    def with_gamma_pdf(cls, basevars, energy_pdf, name, lifetime, live_time):
+        return cls(basevars, energy_pdf, name, lifetime, live_time)
+
     def get_model(self):
         return self.gamma_pdf
 
     def get_lifetime(self):
         return self.lifetime
-        #return self.energy_pdf
+
+    def get_energy_pdf(self):
+        return self.energy_pdf
 
 
 class GammaLineFactory:
-    created = {}
+    created = []
+    save_list = []
 
     @classmethod
-    def generate(cls,mean_value, lifetime_value, basevars):
-      return cls.generate(mean_value, mean_value*0.05, 0.1*mean_value, 0.05*mean_value,
-                   lifetime_value, basevars)
+    def generate(cls, name, mean_value, lifetime_value, basevars):
+        return cls.generate(basevars, name, mean_value=mean_value, mean_error=mean_value*0.05, sigma=0.1*mean_value, sigma_error=0.05*mean_value,
+                   lifetime_value=lifetime_value)
     @classmethod
-    def generate(cls,mean_value, mean_error, sigma, sigma_error, lifetime_value, basevars, name=None, live_time = None):
-        if mean_value in cls.created.keys(): return cls.created[mean_value][0]
-        if not name:
-            name = str(mean_value) + "_" + str(lifetime_value)
-        mean = ROOT.RooRealVar("mean_%s" % name,"mean_%s" % name, 
-                               mean_value, 
-                               mean_value-mean_error, 
-                               mean_value+mean_error)
-        if mean_error == 0:
-            mean.setConstant()
-        sigma = ROOT.RooRealVar("sigma_%s" % name,"sigma_%s" % name, 
-                                sigma, 
-                                sigma - sigma_error, 
-                                sigma + sigma_error)
-        if sigma_error ==0:
-            sigma.setConstant()
+    def generate(cls, basevars, name, **kw):
+        """
+        The inputs to this are as follows:
+        mean_vaue: expected mean value
+        mean_error: expected mean error.  If 0, will be set to constant
+        sigma:
+        sigma_error:
+        lifetime_value: either 
+        lifetime_err: either 
+        gamma_pdf: the gamma_pdf
+        live_time: should be a MGMPiecewiseRegions object
+        """
+        lifetime_value = kw['lifetime_value']
+        lifetime_err   = kw['lifetime_err']
+        live_time      = kw['live_time']
         try:
             lifetime_value.IsA()
             lifetime = lifetime_value
@@ -86,12 +94,39 @@ class GammaLineFactory:
             if lifetime_value == 0:
                 lifetime = None
             else:
+                min_value = lifetime_value - lifetime_err
+                if min_value <= 0: min_value = 1e-16
                 lifetime = ROOT.RooRealVar("lifetime_%s" % name,
                                            "lifetime_%s" % name, 
                                            lifetime_value, 
-                                           0.1*lifetime_value, 
-                                           10*lifetime_value)
-        gamma_line = GammaLineModel(basevars, mean, sigma, lifetime, live_time)
-        cls.created[mean_value] = (gamma_line, mean, sigma, lifetime)
-        return cls.created[mean_value][0]
+                                           min_value, 
+                                           lifetime_value + lifetime_err)
+                if lifetime_err == 0: lifetime.setConstant()
+                cls.save_list.append(lifetime)
+
+        if "gamma_pdf" not in kw.keys():
+            mean_value = kw['mean_value']
+            mean_error = kw['mean_error']
+            mean = ROOT.RooRealVar("mean_%s" % name,"mean_%s" % name, 
+                                   mean_value, 
+                                   mean_value-mean_error, 
+                                   mean_value+mean_error)
+            if mean_error == 0: mean.setConstant()
+            sigma_val = kw['sigma']
+            sigma_error = kw['sigma_error']
+            sigma = ROOT.RooRealVar("sigma_%s" % name,"sigma_%s" % name, 
+                                    sigma_val, 
+                                    sigma_val - sigma_error, 
+                                    sigma_val + sigma_error)
+            cls.save_list.append((mean, sigma))
+
+            if sigma_error ==0:
+                sigma.setConstant()
+
+            gamma_line = GammaLineModel.with_mean_sigma(basevars, mean, sigma, lifetime, live_time)
+        else:
+            gamma_line = GammaLineModel.with_gamma_pdf(basevars, kw['gamma_pdf'], name, lifetime, live_time)
+
+        cls.created.append(gamma_line)
+        return cls.created[-1]
         
--- pyWIMP/DMModels/low_energy_background_time.py
+++ pyWIMP/DMModels/low_energy_background_time.py
@@ -21,16 +21,17 @@ class FittingModelTime(BaseModel):
     def initialize(self, basevars, livetime):
                           #Name  :  Energy  :  EnergyErr  :  SigmaErr :  L/K ratio : L-energy : L-energy error : Livetime : max_ampl 
         mean_list = [
-                     ("73As", 11.103, 0.04, 0.04, 0.104, 1.414, 0.00, 80.30,   500), 
-                     ("68Ge", 10.367, 0.10, 0.04, 0.108, 1.302, 0.00, 270.95,  10000), 
-                     ("68Ga", 9.659,  0.10, 0.04, 0.112, 1.196, 0.00, 100.0,   3000), 
-                     ("65Zn", 8.989,  0.10, 0.04, 0.117, 1.100, 0.00, 243.66,  10000), 
+                     ("73As", 11.103, 0.04, 0.04, 0.104, 1.414, 0.00, 80.30,        20,  200), 
+                     ("68Ge", 10.367, 0.10, 0.04, 0.108, 1.302, 0.00, 270.95,       80, 5000), 
+                     #("68GeFast", 10.367, 0.10, 0.04, 0.108, 1.302, 0.00, 11.26,    0,  10000), 
+                     ("68Ga", 9.659,  0.10, 0.04, 0.112, 1.196, 0.00, 270.95,       100, 600), 
+                     ("65Zn", 8.989,  0.10, 0.04, 0.117, 1.100, 0.00, 243.66,       100, 3000), 
                      #("56Ni", 7.710,  0.2,  0.0,  0.),                        
-                     ("58Co", 7.110,  0.04, 0.0,  0.127, 0.929, 0.00, 70.86,   300),
-                     #("55Fe", 6.540,  0.2,  0.0,  0.133, 0.842, 0.00, 365.25*2.737, 3000), 
-                     ("54Mn", 5.990,  0.10, 0.0,  0.139, 0.754, 0.00, 312.12,  500),
-                     ("51Cr", 5.460,  0.10, 0.0,  0.146, 0.682, 0.00, 27.7025, 300),
-                     ("49V",  4.970,  0.2, 0.0,  0.154, 0.604, 0.00,  300,     500),
+                     ("58Co", 7.010,  0.1, 0.0,  0.127, 0.929,  0.00, 70.86,        20, 200),
+                     ("55Fe", 6.540,  0.2,  0.0,  0.133, 0.842, 0.00, 100*365.25*2.737, 0, 200), 
+                     ("54Mn", 5.990,  0.15, 0.0,  0.139, 0.754, 0.00, 100*312.12,   0, 200),
+                     ("51Cr", 5.460,  0.10, 0.0,  0.146, 0.682, 0.00, 27.7025,      40, 50),
+                     ("49V",  4.970,  0.2, 0.0,  0.154, 0.604,  0.00, 100*300,          0, 200),
                     ]
                     #("Unknown", 0.9, 0.05, 0.1, 0.04, 0), 
         
@@ -40,19 +41,41 @@ class FittingModelTime(BaseModel):
         self.low_pdf_list = ROOT.RooArgList()
         self.low_coefficienct_list = ROOT.RooArgList()
 
-        for name, mean, mean_error, sigma_error, l_k_ratio, l_energy, l_energy_err, decay_time, max_ampl in mean_list:
-            agamma = gamma_line_model.GammaLineFactory.generate(mean, 
-                        mean_error, get_sigma(mean*1e3)*1e-3, 
-                        sigma_error, decay_time, basevars,name, livetime)
-            agamma_low = gamma_line_model.GammaLineFactory.generate(l_energy, 
-                        l_energy_err, get_sigma(l_energy*1e3)*1e-3, 
-                        0.0, agamma.get_lifetime(), basevars,name+"_l_line", livetime)
+        ga68_time_pdf = None
+        for name, mean, mean_error, sigma_error, l_k_ratio, l_energy, l_energy_err, decay_time, decay_err, max_ampl in mean_list:
+            if name == "68Ga": 
+                decay_time = ga68_time_pdf
+                decay_err = None
+
+            agamma = gamma_line_model.GammaLineFactory.generate(
+                        basevars,name, 
+                        mean_value=mean, 
+                        mean_error=mean_error, 
+                        sigma=get_sigma(mean*1e3)*1e-3, 
+                        sigma_error=sigma_error, 
+                        lifetime_value=decay_time, 
+                        lifetime_err=decay_err, 
+                        live_time=livetime)
+
+            next_life = agamma.get_lifetime()
+            if name == "68Ge": ga68_time_pdf = next_life 
+            if name == "68Ga": next_life = ga68_time_pdf
+
+            agamma_low = gamma_line_model.GammaLineFactory.generate(
+                        basevars,name+"_l_line", 
+                        mean_value=l_energy, 
+                        mean_error=l_energy_err, 
+                        sigma=get_sigma(l_energy*1e3)*1e-3, 
+                        sigma_error=0.0, 
+                        lifetime_value=next_life, 
+                        lifetime_err=None, 
+                        live_time=livetime)
 
             gamma = agamma.get_model()
             low_gamma = agamma_low.get_model()
             the_name = "%s_ampl" % gamma.GetName()
             new_var = ROOT.RooRealVar(the_name, the_name, 
-                                      1e-15, max_ampl, "Counts")
+                                      5, max_ampl, "Counts")
             low_ampl = ROOT.RooFormulaVar(the_name + "_low", the_name + "_low", "%f*@0" % l_k_ratio, ROOT.RooArgList(new_var))
 
             self.save_list.append((agamma, agamma_low, new_var, low_ampl))
@@ -65,30 +88,37 @@ class FittingModelTime(BaseModel):
         tag = "high"
         flat_coef = ROOT.RooRealVar("flat_coef_%s" % tag,
                                     "flat_coef_%s" % tag,
-                                    10, 4000)
+                                    2000, 10, 4000)
         # Flat pdf
-        energy_pdf_flat = ROOT.RooPolynomial("energy_pdf_flat_%s" % tag, 
-                                             "energy_pdf_flat_%s" % tag, 
-                                             basevars.get_energy())
+        time_pdf_flat = pdfs.MGMPiecewiseFunction("time_pdf_flat_all", "time_pdf_flat_all", self.basevars.get_time())
+        time_pdf_flat.SetRegionsOfValidity(livetime) 
+
+        time_exp = ROOT.RooRealVar("time_exp", "time_exp", -0.01, -1, -1e-16)
+        time_pdf_exp = pdfs.MGMExponential("time_pdf_exp", "time_pdf_exp", self.basevars.get_time(), time_exp)
+        time_pdf_exp.SetRegionsOfValidity(livetime) 
+
         energy_pdf_flat = pdfs.MGMPiecewiseFunction("energy_pdf_flat_%s" % tag,  "energy_pdf_flat_%s" % tag, self.basevars.get_energy())
         energy_pdf_flat_regions = pdfs.MGMPiecewiseRegions()
-        energy_pdf_flat_regions.InsertNewRegion(3.1760041, 14.6620021)
+        energy_pdf_flat_regions.InsertNewRegion(2.5, 15)
         energy_pdf_flat.SetRegionsOfValidity(energy_pdf_flat_regions)
-        asig = ROOT.RooRealVar("asig", "asig", 0.1)
-        amean = ROOT.RooRealVar("amean", "amean", 10.367)
-        anoffset = ROOT.RooRealVar("anoffset", "anoffset", 0)
-        erf_amp = ROOT.RooRealVar("erf_amp", "erf_amp", 1e-15, 6000)
-        erf_pdf = pdfs.MGMErfcFunction("erf_func", "erf_func", basevars.get_energy(), amean, asig, anoffset)
+        energy_flat_whole = ROOT.RooProdPdf("high_flat_whole", "high_flat_whole", energy_pdf_flat, time_pdf_flat)
+
+        erf_amp = ROOT.RooRealVar("erf_amp", "erf_amp", 1000, 10, 6000)
         erf_pdf = pdfs.MGMPiecewiseFunction("erf_func",  "erf_func", self.basevars.get_energy())
         erf_pdf_flat_regions = pdfs.MGMPiecewiseRegions()
-        erf_pdf_flat_regions.InsertNewRegion(3.1760041, 10.367)
-        erf_pdf.SetRegionsOfValidity( erf_pdf_flat_regions)
+        erf_pdf_flat_regions.InsertNewRegion(2.5, 10.7) 
+        erf_pdf.SetRegionsOfValidity(erf_pdf_flat_regions)
+        #erf_pdf_whole = ROOT.RooProdPdf("high_erf_whole", "high_erf_whole", erf_pdf, time_pdf_flat)
+        erf_pdf_whole = ROOT.RooProdPdf("high_erf_whole", "high_erf_whole", erf_pdf, time_pdf_exp)
 
-        self.save_list.extend([flat_coef, energy_pdf_flat, asig, amean, anoffset, erf_amp, erf_pdf, energy_pdf_flat_regions, erf_pdf_flat_regions])
+        self.save_list.extend([flat_coef, energy_pdf_flat, erf_amp, erf_pdf, energy_pdf_flat_regions, erf_pdf_flat_regions])
+        self.save_list.extend([time_pdf_flat, energy_flat_whole, erf_pdf_whole])
+        self.save_list.extend([time_pdf_exp, time_exp])
 
-        self.high_pdf_list.add(energy_pdf_flat)
+        self.high_pdf_list.add(energy_flat_whole)
         self.high_coefficienct_list.add(flat_coef)
-        self.high_pdf_list.add(erf_pdf)
+
+        self.high_pdf_list.add(erf_pdf_whole)
         self.high_coefficienct_list.add(erf_amp)
 
         self.high_extend_list = ROOT.RooArgList()
@@ -105,14 +135,14 @@ class FittingModelTime(BaseModel):
         tag = "low"
         exp_constant_one = ROOT.RooRealVar("expo_const_one%s" % tag,
                                            "expo_const_one%s" % tag,
-                                           -3., -10, -0.8)
+                                           -3., -5, -0.8)
         exp_constant_one.setError(0.5)
         exp_coef = ROOT.RooRealVar("exp_coef_%s" % tag,
                                    "exp_coef_%s" % tag,
-                                   10, 3000)
+                                   600, 10, 1000)
         low_flat_coef = ROOT.RooRealVar("flat_coef_%s" % tag,
                                         "flat_coef_%s" % tag,
-                                        10, 3000)
+                                        1000, 10, 2000)
         # Flat pdf
         low_erf_sig = ROOT.RooRealVar("low_sig", "low_sig", 0)
         low_erf_mean = ROOT.RooRealVar("low_erf_mean", "low_erf_mean", self.max_energy)
@@ -123,24 +153,37 @@ class FittingModelTime(BaseModel):
                                             low_erf_sig, 
                                             low_erf_offset)
 
+        self.phase    = ROOT.RooRealVar("phase", "phase", 0, -ROOT.TMath.Pi(), ROOT.TMath.Pi()) 
+        self.osc_ampl = ROOT.RooRealVar("osc_amplitude", "osc_amplitude", 0, 1.) 
+        self.frequency = ROOT.RooRealVar("osc_frequency", "osc_frequency", 1., 0.1, 10.) 
+        self.frequency.setConstant(True)
+        osc_time_pdf = ROOT.MGMExponentialPlusSinusoid("osc_pdf", "osc_pdf", 
+                                                       self.basevars.get_time(), time_exp, 
+                                                       self.osc_ampl, 
+                                                       self.frequency, 
+                                                       self.phase) 
+        osc_time_pdf.SetRegionsOfValidity(livetime)
+        self.save_list.append(osc_time_pdf)
+
+
         energy_exp_pdf = ROOT.RooExponential("energy_pdf_exp", 
                                              "energy_pdf_exp", 
                                              basevars.get_energy(),
                                              exp_constant_one)
 
-        self.low_pdf_list.add(low_erf_pdf)
+        exp_pdf_whole = ROOT.RooProdPdf("exp_pdf_whole", "exp_pdf_whole", energy_exp_pdf, osc_time_pdf)
+        low_erf_whole = ROOT.RooProdPdf("low_erf_whole", "low_erf_whole", low_erf_pdf, time_pdf_exp)
+
+        self.low_pdf_list.add(low_erf_whole)
         self.low_coefficienct_list.add(low_flat_coef)
 
-        self.low_pdf_list.add(energy_exp_pdf)
+        self.low_pdf_list.add(exp_pdf_whole)
         self.low_coefficienct_list.add(exp_coef)
 
         self.save_list.extend([energy_exp_pdf, low_erf_pdf, low_erf_offset, low_erf_mean, low_erf_sig,
                                low_flat_coef, exp_coef, exp_constant_one])
+        self.save_list.extend([low_erf_whole, exp_pdf_whole])
 
-        self.time_efficiency = pdfs.MGMPiecewiseFunction("time_eff", "time_eff", self.basevars.get_time())
-        #self.energy_range = pdfs.MGMPiecewiseFunction("energy_range", "energy_range", self.basevars.get_energy())
-        #self.high_energy_range = pdfs.MGMPiecewiseFunction("high_energy_range", "high_energy_range", self.basevars.get_energy())
-        
         self.low_extend_list = ROOT.RooArgList()
         for i in range(self.low_pdf_list.getSize()): 
             pdf, coeff = self.low_pdf_list.at(i), self.low_coefficienct_list.at(i)
@@ -150,21 +193,26 @@ class FittingModelTime(BaseModel):
 
 
 
-    def get_high_components(self):
-        return self.high_pdf_list, self.high_coefficienct_list
-
-    def get_low_components(self):
-        return self.low_pdf_list, self.low_coefficienct_list
-
     def get_extend_components(self):
         return self.low_extend_list, self.high_extend_list
 
-    def get_time_efficiency(self):
-        return self.time_efficiency
+    def get_osc_ampl(self):
+        return self.osc_ampl
 
-    def get_energy_ranges(self):
-        return self.energy_range, self.high_energy_range
+    def set_frequency(self, val):
+        self.frequency.setVal(val)
 
+    def set_osc_ampl_const(self, val):
+        self.set_osc_ampl(val)
+        self.osc_ampl.setConstant(True)
 
+    def set_osc_ampl(self, val):
+        self.osc_ampl.setVal(val)
+        if val == 0.0:
+            self.osc_ampl.setConstant(True)
+            self.phase.setConstant(True)
+        else:
+            self.osc_ampl.setConstant(False)
+            self.phase.setConstant(False)
 
 
